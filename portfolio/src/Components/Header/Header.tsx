58848bc3c705175f0841d508b92393c031379da6
Eliezer Nascimento <eliezergarbin1@gmail.com>
2022-12-22 23:35:49 -0300

    iniciando header

---
+++ portfolio/src/Components/Header/Header.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+        <header className='header'>
+            <div className='container d_flex'>
+                <div className='logo'>
+                    <img src="" alt="" />
+                </div>
+
+                <div className='navlink'>
+                    <ul className='link f_flex uppercase'>
+                        <li></li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+    </>
+  )
+}
+
+export default Header
