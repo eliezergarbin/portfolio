203b984accff7a23283e39dd5b13c1748edaef5f
Eliezer Nascimento <eliezergarbin1@gmail.com>
2022-12-24 17:32:24 -0300

    finalizando estrutura do header

--- portfolio/src/Components/Header/Header.tsx
+++ portfolio/src/Components/Header/Header.tsx
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
+import logo from "../pics/logo.png";
 
 const Header = () => {
+  window.addEventListener("scroll", function () {
+    const header: any = document.querySelector(".header");
+    header.classList.toggle("active", window.scrollY > 100);
+  });
+
+  const [Mobile, setMobile] = useState(false);
   return (
     <>
       <header className="header">
         <div className="container d_flex">
           <div className="logo">
-            <img src="" alt="" />
+            <img src={logo} alt="" />
           </div>
 
           <div className="navlink">
-            <ul className="link f_flex uppercase">
+            <ul
+              className={Mobile ? "nav-links-mobile" : "link f_flex uppercase"}
+              onClick={() => setMobile(false)}
+            >
               <li>
                 <a href="#home">home</a>
               </li>
@@ -34,12 +44,16 @@ const Header = () => {
                 <a href="#contact">contact</a>
               </li>
               <li>
-                <button className="home-btn">BUY NOW</button>
+                <button className="home-btn">BUTTON TEST</button>
               </li>
             </ul>
 
-            <button className="toggle">
-                <i className="fas fa-times"></i>
+            <button className="toggle" onClick={() => setMobile(!Mobile)}>
+              {Mobile ? (
+                <i className="fas fa-times close home-btn"></i>
+              ) : (
+                <i className="fas fa-bars open"></i>
+              )}
             </button>
           </div>
         </div>
